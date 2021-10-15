da8336ce862a3b3349540f15ab08e22136526ee2
Nick <dadxxxanders@gmail.com>
2021-10-15 15:12:11 +0300

    3. Avatar handler (handler adds a watermark at the model level)

--- FindFriend/users/models.py
+++ FindFriend/users/models.py
@@ -1,9 +1,22 @@
+from PIL import Image
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 # username_validator = UnicodeUsernameValidator()
+from imagekit.models import ImageSpecField, ProcessedImageField
+from pilkit.processors import ResizeToFill
+
 from users.manager import CustomUserManager
+
+
+class Watermark(object):
+    def process(self, image):
+        # img = Image.open('123.png')
+        watermark = Image.open('watermark.png').convert("RGBA")
+        image.paste(watermark, (0, 0), watermark)
+        # image.save(self)
+        return image
 
 
 class CustomUser(AbstractUser):
@@ -14,7 +27,17 @@ class CustomUser(AbstractUser):
     )
     username = None
     email = models.EmailField(_('email address'), blank=False, unique=True)
-    avatar = models.ImageField(upload_to="avatars/%Y/%m/%d/", blank=True)
+    # avatar = models.ImageField(upload_to='avatars/%Y/%m/%d/', blank=True)
+    avatar = ProcessedImageField(upload_to='avatars/%Y/%m/%d/',
+                                 processors=[
+                                     ResizeToFill(500, 500),
+                                     Watermark(),
+                                 ],
+                                 format='PNG',
+                                 blank=True
+                                 # options={'quality': 60}
+                                 )
+
     sex = models.BooleanField(choices=SEX_CHOICES, blank=True, null=True, default=None)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ()
